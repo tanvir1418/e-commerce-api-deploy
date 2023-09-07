179b17ef105b08194d088c4d4093d28816863a14
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2023-09-07 07:14:55 +0600

    Initial Commit

---
+++ models/Product.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+const Review = require('./Review');
+
+const ProductSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: [true, 'Please provide product name'],
+            maxLength: [100, 'Name can not be more than 100 characters'],
+        },
+        price: {
+            type: Number,
+            required: [true, 'Please provide product price'],
+            default: 0,
+        },
+        description: {
+            type: String,
+            required: [true, 'Please provide product description'],
+            maxLength: [
+                1000,
+                'Description can not be more than 1000 characters',
+            ],
+        },
+        image: {
+            type: String,
+            default: '/uploads/example.jpeg',
+        },
+        category: {
+            type: String,
+            required: [true, 'Please provide product category'],
+            enum: ['office', 'kitchen', 'bedroom'],
+        },
+        company: {
+            type: String,
+            required: [true, 'Please provide company'],
+            enum: {
+                values: ['ikea', 'liddy', 'marcos'],
+                message: '{VALUE} is not supported',
+            },
+        },
+        colors: {
+            type: [String],
+            default: ['#222'],
+            required: true,
+        },
+        featured: {
+            type: Boolean,
+            default: false,
+        },
+        freeShipping: {
+            type: Boolean,
+            default: false,
+        },
+        inventory: {
+            type: Number,
+            required: true,
+            default: 15,
+        },
+        averageRating: {
+            type: Number,
+            default: 0,
+        },
+        numOfReviews: {
+            type: Number,
+            default: 0,
+        },
+        user: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Users',
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
+
+ProductSchema.virtual('reviews', {
+    ref: 'Reviews',
+    localField: '_id',
+    foreignField: 'product',
+    justOne: false,
+    // match: { rating: 5 }, // if rating is 5 then select data
+});
+
+ProductSchema.pre('deleteOne', async function () {
+    const productQuery = this.getFilter();
+    await Review.deleteMany({ product: productQuery._id });
+});
+
+module.exports = mongoose.model('Products', ProductSchema);
